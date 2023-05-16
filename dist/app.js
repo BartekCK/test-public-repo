41fa3107cdadd4fb4d0d029555cf671991d04a78
Bartek <>
2023-05-16 21:29:55 +0200

    test

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.start = void 0;
-var start = function () {
-    console.log("Hello World");
-};
-exports.start = start;
